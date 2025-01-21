5b085bed86f1cccfa16cfd489564f13c0b577359
Joalor64 <rhgarcon@gmail.com>
2025-01-21 14:02:51 -0500

    fix + remove archive comments

--- js/swfobject.js
+++ js/swfobject.js
@@ -689,24 +689,3 @@ var swfobject = (function () {
       },
   };
 })();
-/*
-   FILE ARCHIVED ON 03:32:47 Sep 04, 2016 AND RETRIEVED FROM THE
-   INTERNET ARCHIVE ON 04:48:36 Feb 14, 2020.
-   JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
-
-   ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
-   SECTION 108(a)(3)).
-*/
-/*
-playback timings (ms):
-RedisCDXSource: 0.752
-exclusion.robots: 0.187
-CDXLines.iter: 13.363 (3)
-LoadShardBlock: 211.405 (3)
-exclusion.robots.policy: 0.174
-captures_list: 231.854
-PetaboxLoader3.datanode: 263.309 (5)
-esindex: 0.013
-PetaboxLoader3.resolve: 70.877 (2)
-load_resource: 132.308
-*/
